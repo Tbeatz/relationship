340401d58968be0d62f677bab9f1ec8e2c111cd2
Thu Ta <61737224+Tbeatz@users.noreply.github.com>
2023-10-13 13:26:55 +0630

    new field add

--- resources/views/livewire/register.blade.php
+++ resources/views/livewire/register.blade.php
@@ -16,6 +16,11 @@
                 </div>
                 <form class="space-y-4 md:space-y-6">
                     <div>
+                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white font-arial">Your name</label>
+                        <input wire:model='name' type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 font-arial" placeholder="Name" required="">
+                        @error('name') <span class="mt-1 text-red-500 text-sm font-arial">{{ $message }}</span> @enderror
+                    </div>
+                    <div>
                         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white font-arial">Your email</label>
                         <input wire:model='email' type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 font-arial" placeholder="name@gmail.com" required="">
                         @error('email') <span class="mt-1 text-red-500 text-sm font-arial">{{ $message }}</span> @enderror
